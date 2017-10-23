5da10cb38a2bd2bf8527f440d56f78efcab87697
Rafael Paiva <rpneves@dei.uc.pt>
2017-10-23 17:05:17 +0100

    second commit

--- src/interfaces/VotoInt.java
+++ src/interfaces/VotoInt.java
@@ -1,7 +1,10 @@
 package interfaces;
 
+import java.rmi.RemoteException;
+
 /**
  * Created by Carlos on 21-10-2017.
  */
 public interface Voto extends ModelInt {
+    String inLinePrint() throws RemoteException;
 }
